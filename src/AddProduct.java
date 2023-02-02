438b462ee61aefc3874f1f79a8abd11516a0d654
ferjoo <ferjomor@gmail.com>
2023-02-01 20:57:48 -0600

    Adding validation to check if the product id already exists

--- src/AddProduct.java
+++ src/AddProduct.java
@@ -7,15 +7,33 @@ public class AddProduct {
         System.out.println("Ingrese el precio del producto");
         double price = scanner.nextDouble();
         // create a new product
+        // check the price is higher than 0
+        while (price <= 0) {
+            System.out.println("El precio debe ser mayor a 0");
+            price = scanner.nextDouble();
+        }
         System.out.println("Ingrese el id del producto");
         int id = scanner.nextInt();
         product newProduct = new product(name, price, id, 0);
-        // if the id dont exist add the product to the cart
+        // check if the id already exists
+        boolean idAlreadyExists = false;
         for (int i = 0; i < Main.products.length; i++) {
-            if (Main.products[i] == null) {
-                Main.products[i] = newProduct;
-                break;
+            if (Main.products[i] != null) {
+                if (Main.products[i].getId() == id) {
+                    idAlreadyExists = true;
+                }
             }
+        }
+        // if the id dont exist add the product to the cart
+        if (!idAlreadyExists) {
+            for (int i = 0; i < Main.products.length; i++) {
+                if (Main.products[i] == null) {
+                    Main.products[i] = newProduct;
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Ya existe un producto con ese identificador");
         }
 
     }
